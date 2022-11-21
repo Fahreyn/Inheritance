cfef539f25473cc2e59858de05e5911060dc7c2d
selal <selal@10.22.68.183>
2022-11-21 13:29:45 +0300

    Inheritance

---
+++ inheritance/src/inheritance/Main.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		IndividualCustomer engin = new IndividualCustomer();
+		engin.customerNumber = "12345";
+
+		CorporateCustomer hepsiBurada = new CorporateCustomer();
+		hepsiBurada.customerNumber = "78910";
+
+	
+		
+		SendikaCustomer sendikaCustomer = new SendikaCustomer();
+		sendikaCustomer.customerNumber = "99999";
+		
+		CustomerManager customerManager = new CustomerManager();
+
+		Customer[] customers = {engin,sendikaCustomer,hepsiBurada};
+		
+		customerManager.addMultiple(customers);
+      //polymorphism classlarda extend eden clasýn diðer clasýn referansýný tutabilmesidir
+
+	}
+
+}
---
+++ inheritance/src/inheritance/SendikaCustomer.java
@@ -0,0 +1,6 @@
+package inheritance;
+
+public class SendikaCustomer extends Customer {
+		String sendikanumarasý;
+}
+//plug play(koda birþeyler eklemek istediðimizde mevcut kodun ana yapýsýný deðiþtirmememiz gerekir)
